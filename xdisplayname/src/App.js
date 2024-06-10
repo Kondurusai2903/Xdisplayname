063c5efb45e2736a3a900495ac5f763a910d5f16
kondurusai <kondurusai863@gmail.com>
2024-06-10 20:55:42 +0530

    xdisplaydone

---
+++ xdisplayname/src/App.js
@@ -0,0 +1,11 @@
+import Display from "./component/display";
+
+function App() {
+  return (
+    <div className="App">
+      <Display />
+    </div>
+  );
+}
+
+export default App;
---
+++ xdisplayname/src/component/display.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+const Display = () => {
+  const [user, setUser] = useState({
+    firstname: "",
+    secondname: "",
+  });
+  const [flag, setFlag] = useState(false);
+  const handlechange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setUser((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    setFlag(!flag);
+  };
+  console.log(flag);
+  console.log(user);
+  return (
+    <div>
+      <h1>Full Name Display</h1>
+      <form>
+        <div>
+          <label for="firstname">First Name:</label>
+          <input
+            type="text"
+            name="firstname"
+            // value={e.target.value}
+            onChange={handlechange}
+            required
+          />
+        </div>
+        <div>
+          <label for="lastname">Last Name:</label>
+          <input
+            type="text"
+            name="secondname"
+            onChange={handlechange}
+            required
+          />
+        </div>
+        <button type="submit" onClick={handlesubmit}>
+          Submit
+        </button>
+      </form>
+      {/* <p>{flag ? "Full Name:"+{user.firstname}+{user.secondname} : ""}</p> */}
+      <p>
+        {flag ? "Full Name: " + user.firstname + " " + user.secondname : ""}
+      </p>
+    </div>
+  );
+};
+
+export default Display;
